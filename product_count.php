34bbefcbd20a1feba95d38a9b66691a5f46691e2
info3g <infothreeg@gmail.com>
2018-05-08 15:29:45 +0530

    Update product_count.php

--- product_count.php
+++ product_count.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $access_token = $_REQUEST['access_token'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
-$product_count = $shopify('GET /admin/products/count.json');
+$product_count = $shopify('GET /admin/collects/count.json');
 	$limit=20; // Number of product per page
 	$noofPage s= $product_count/$limit;
 	 echo $noofPages = ceil($noofPages);
