75591fa210e91268987e27494f08a5aa6eda5de5
info3g <infothreeg@gmail.com>
2018-05-08 15:12:50 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -5,7 +5,7 @@ use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
-{
+{       $collections = $shopify('GET /admin/collects.json');
 	$collections = $shopify('GET /admin/collects.json');
 	if($collections){
 		echo '<form method="post" name="form" id="getproducts" action="#">';
