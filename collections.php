9420d883f5eedaee00220773bf9da30608923706
info3g <infothreeg@gmail.com>
2018-05-08 13:25:11 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -9,25 +9,7 @@ try
 	      
 			$products = $shopify('GET /admin/collections.json');
 	print_r($products);
-			if($products){
-			echo '<form method="post" name="form" id="getproducts" action="#">';
-			echo '<table cellspacing="10" cellpadding="10" border="1">';
-			echo '<thead><tr><th></th><th>Product Name</th><th>Content</th><th>Brand</th><th>Image</th></tr></thead>';
-			echo '<tbody>';
-			foreach($products as $Allproducts) {
-				echo '<tr><td><input type="checkbox" name="product_ids[]" value="'.$Allproducts["id"].'" data-pro-handle="'.$Allproducts["handle"].'" /></td>';
-				echo '<td>'.$Allproducts['title'].'</td>';
-				echo '<td>'.$Allproducts['body_html'].'</td>';
-				echo '<td>'.$Allproducts['vendor'].'</td>';
-				echo '<td><img src="'.$Allproducts["image"]["src"].'" alt="productimage" /></td></tr>';
-			}
-			echo '<tr><td colspan="5"><input type="button" class="saveproducts" value="Show button on Product Page" name="submit" /></td></tr></tbody>';
-			echo '</table>';
-			 echo '</form>';
-			}
-	else{
-	echo "<div class='no-result'>No Products</div>";
-	}
+			
 }
 catch (shopify\ApiException $e)
 {
