c3ba7f55c95a8c700c26155779e270e0149ee13b
info3g <infothreeg@gmail.com>
2018-05-08 16:20:07 +0530

    Update getmetafields.php

--- getmetafields.php
+++ getmetafields.php
@@ -6,7 +6,7 @@ $access_token = $_REQUEST['access_token'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {		
-		$response = $shopify('GET /admin/metafields.json');
+		$response = $shopify('GET /admin/collections/metafields.json');
 		$revisebutton = ''; $automanualfield = '';
 		foreach($response as $options){
 			if($options['namespace'] == 'revisebutton'){
