f6806ff51b338ceec23f1e8cd972b290e6ddb133
info3g <infothreeg@gmail.com>
2018-05-08 12:49:21 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
        
-	define('SHOPIFY_APP_API_KEY', 'e54beaa8ca92d08d6519ad835300ba0a');
-	define('SHOPIFY_APP_SHARED_SECRET', '821bdc7bdd58060e2caffffb7a7f1221');
+	define('SHOPIFY_APP_API_KEY', 'a1d8a4ace6eaa1e0241afa2dcd19132c');
+	define('SHOPIFY_APP_SHARED_SECRET', 'fdf3bce4b18ab3c6bf490118795dda47');
 	
 ?>
 
