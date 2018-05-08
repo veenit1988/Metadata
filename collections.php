515b141f851e06e4d22234acb9a2397864cb6052
info3g <infothreeg@gmail.com>
2018-05-08 17:50:48 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -9,7 +9,7 @@ try
 		if($collections){
 		echo '<form method="post" name="form" id="getproducts" action="#">';
 		echo '<table cellspacing="10" cellpadding="10" border="1">';
-		echo '<thead><tr><th></th><th>Collection Name</th><th>Image</th><th>Content</th></tr></thead>';
+		echo '<thead><tr><th></th><th>Collection Name</th><th>Image</th><th>Content</th><th>Add Content</th></tr></thead>';
 			echo '<tbody>';
 		foreach($collections as $Allcollections)
 		
@@ -19,6 +19,7 @@ try
 			echo '<td>'.$Allcollections['title'].'</td>';
 			echo '<td><img src="'.$Allcollections["image"]["src"].'" alt="collectionimage" /></td>';
 			echo '<td>'.$Allcollections['body_html'].'</td>';
+			echo '<td>'<input type="button" class="editcollectionbtn" value="add field" >'</td>';
 			echo '</tr>';
 			
 			}
