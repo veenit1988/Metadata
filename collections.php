4ebce64f21e3354fa878f6562db5c068e35eae13
info3g <infothreeg@gmail.com>
2018-05-08 18:55:23 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -19,7 +19,7 @@ try
 			echo '<td>'.$Allcollections['title'].'</td>';
 			echo '<td><img src="'.$Allcollections["image"]["src"].'" alt="collectionimage" /></td>';
 			echo '<td>'.$Allcollections['body_html'].'</td>';
-			echo '<td>'.'<input type="button" class="editcollectionbtn" value="add field" onclick="metaform(event);"/>'.'</td>';
+			echo '<td>'.'<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#gridSystemModal"> Add Metafield</button>'.'</td>';
 			echo '</tr>';
 			
 			}
@@ -47,3 +47,43 @@ function metaform(e) {
    alert("hi");
 }
 </script>
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-4">.col-md-4</div>
+          <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+          <div class="col-md-2 col-md-offset-4">.col-md-2 .col-md-offset-4</div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
+        </div>
+        <div class="row">
+          <div class="col-sm-9">
+            Level 1: .col-sm-9
+            <div class="row">
+              <div class="col-xs-8 col-sm-6">
+                Level 2: .col-xs-8 .col-sm-6
+              </div>
+              <div class="col-xs-4 col-sm-6">
+                Level 2: .col-xs-4 .col-sm-6
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
