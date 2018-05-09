3ad0f3cea40c35ff4ff2894d5282f6f12a8589b6
info3g <infothreeg@gmail.com>
2018-05-09 12:01:31 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -21,7 +21,7 @@ try
 			echo '<td>'.$Allcollections['body_html'].'</td>';
 			echo '<td>'.'<textarea class="form-control" id="metafield1-text" name="sel_options[]"></textarea>'.'</td>';
 			echo '<td>'.'<textarea class="form-control" id="metafield2-text" name="sel_options[]"></textarea>'.'</td>';
-			echo '<td>'.'<button type="button" class="btn btn-primary collectionbtn">Add Metafield</button>'.'</td>';
+			echo '<td>'.'<a href="#" id="'.$Allcollections["id"].'">Add Metafield</a>'.'</td>';
 			echo '</tr>';
 			
 			}
