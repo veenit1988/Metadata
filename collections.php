8108afc9f09277b38c7642245eba204fcc839ea3
info3g <infothreeg@gmail.com>
2018-05-09 11:33:24 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -19,6 +19,8 @@ try
 			echo '<td>'.$Allcollections['title'].'</td>';
 			echo '<td><img src="'.$Allcollections["image"]["src"].'" alt="collectionimage" /></td>';
 			echo '<td>'.$Allcollections['body_html'].'</td>';
+			echo '<td>'.'<textarea class="form-control" id="metafield1-text"></textarea>'.'</td>';
+			echo '<td>'.'<textarea class="form-control" id="metafield2-text"></textarea>'.'</td>';
 			echo '<td>'.'<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Add Metafield</button>'.'</td>';
 			echo '</tr>';
 			
@@ -41,40 +43,3 @@ catch (shopify\ApiException $e)
 	print_r($e->getResponse());
 }
 ?>
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="exampleModalLabel">Collection Name</h4>
-      </div>
-      <div class="modal-body">
-         <form>
-          <div class="form-group">
-            <label for="recipient-name" class="control-label">Meta field text</label>
-            <input type="text" class="form-control" id="metafield1-text">
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="control-label">Meta field text</label>
-            <textarea class="form-control" id="metafield2-text"></textarea>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save</button>
-      </div>
-    </div>
-  </div>
-</div>
-<script>
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
- // modal.find('.modal-title').text('New message to ' + recipient)
-  //modal.find('.modal-body input').val(recipient)
-})
-</script>
