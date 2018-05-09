37a714971d2c926be25e7d9929d1055fc910a329
info3g <infothreeg@gmail.com>
2018-05-09 16:51:49 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -4,23 +4,25 @@ require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
 echo $collectionid = $_REQUEST['collectionid'];
+echo $meta1 = $_REQUEST[' meta1'];
+echo $meta2 = $_REQUEST[' meta2'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
-	if($alloptions){
-	$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'seloptions', 'value' => $alloptions,
+	if($meta1)
+	{
+	$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'lowerData', 'value' => $meta1,
 	'value_type' => 'string'));
-	} else {
-	$alloptions = "noData";
-	$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'seloptions', 'value' => $alloptions,
+	} 
+	else {
+	$meta1 = "noData";
+	$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'lowerData', 'value' => $meta1,
 	'value_type' => 'string'));
 	}
 	
 	$auto_manual_field = array( "metafield" => array('namespace' => 'automanualfield', 'key' => 'automanual', 'value' => $auto_manual,
-	'value_type' => 'string'));
-	
-	$response = $shopify('POST /admin/collections/' + $collectionid + '/metafields.json',$metafield);
-	
+	'value_type' => 'string'));	
+	$response = $shopify('POST /admin/collections/' + $collectionid + '/metafields.json',$metafield);	
 	$response_auto_manual = $shopify('POST /admin/collections/' + $collectionid + '/metafields.json',$auto_manual_field);
 	echo $response['value'].'==='.$response_auto_manual['value'];
 }
