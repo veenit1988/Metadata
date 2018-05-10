6174e9e2e955e830f872ed1a81ea02c398bdf1a5
info3g <infothreeg@gmail.com>
2018-05-10 10:47:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -88,15 +88,13 @@ if(meta1 != '' || meta2 != '')	{
 }
 
 });	
-
 /*********************fetchdata********************************/
 function fetchMetafield(){
 	console.log('fetch Metafield');
-	var access_token = '<?php echo $access_token ?>';
-	var shop = '<?php echo $_REQUEST['shop'] ?>';
 	$.ajax({
-		url: '/getmetafields.php?access_token='+access_token+'&shop='+shop+'&collectionid='+colId+'&meta1='+meta1+'&meta2='+meta2,
+		url: '/getmetafields.php?access_token='+access_token+'&shop='+shop+'&collectionid='+colId+'&meta2='+meta2,
 		success: function(data){
+			
 			console.log(data);
 		}
 	});
