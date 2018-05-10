5727d57f1304c4db2b5e9fbf8d6eaebbfab2c3fd
info3g <infothreeg@gmail.com>
2018-05-10 18:10:33 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -5,15 +5,12 @@ use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
 $collectionid = $_REQUEST['collectionid'];
 $metafieldData = $_REQUEST['metafieldData'];
-echo SHOPIFY_APP_API_KEY;
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
-	print_r($shopify);
 	$metafield = array( "metafield" => array('namespace' => 'collectionlower', 'key' => 'lowerdata', 'value' => $metafieldData, 'value_type' => 'string'));
-	print_r($metafield);
 	//Collection Metafield
-	$response = $shopify('POST /admin/collections/'.$collectionid.'/metafields.json',$metafield);
+	$response = $shopify('POST /admin/collections/38781747260/metafields.json',$metafield);
 	print_r($response);
 	
 }
