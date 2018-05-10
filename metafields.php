fe5625424173c5424d683499a775aec043ae9baf
info3g <infothreeg@gmail.com>
2018-05-10 19:10:59 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -8,9 +8,8 @@ $metafieldData = $_REQUEST['metafieldData'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
-	$metafield = array( "metafield" => array("namespace" => "collectionlower", "key" => "lowerdata", "value" => "dvdfvfdvdf", "value_type" => "string"));
+	$metafield = array( "metafield" => array("namespace" => "collectionlower", "key" => "lowerdata", "value" => $metafieldData, "value_type" => "string"));
 	//Collection Metafield
-	echo json_encode($metafield);
 	$response = $shopify('POST /admin/collections/'.$collectionid.'/metafields.json',$metafield);
 	print_r($response);
 	
