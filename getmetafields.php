1c62eb8caa551ce442e57cab5ea083648d5ea774
info3g <infothreeg@gmail.com>
2018-05-10 10:34:23 +0530

    Update getmetafields.php

--- getmetafields.php
+++ getmetafields.php
@@ -3,13 +3,12 @@ require __DIR__.'/conf.php'; //Configuration
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
-$collectionid = $_REQUEST['collectionid'];
-$meta2 = $_REQUEST['meta2'];
+echo $collectionid = $_REQUEST['collectionid'];
+echo $meta2 = $_REQUEST['meta2'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {		
-		$response = $shopify('GET /admin/collections' + $collectionid + '/metafields.json');
-	
+	$response = $shopify('GET /admin/collections' + $collectionid + '/metafields.json');
 	print_r($response);
 		$collectiondescription = ''; $lowercollectionmeta = '';
 		foreach($response as $meta2){
