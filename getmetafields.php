42993a655a5e3b7edb90482b6ea8bedad2d7240d
info3g <infothreeg@gmail.com>
2018-05-10 10:38:07 +0530

    Update getmetafields.php

--- getmetafields.php
+++ getmetafields.php
@@ -8,7 +8,7 @@ echo $meta2 = $_REQUEST['meta2'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {		
-	$response = $shopify('GET /admin/collections' + $collectionid + '/metafields.json');
+	$response = $shopify('GET /admin/custom_collections' + $collectionid + '/metafields.json');
 	print_r($response);
 		$collectiondescription = ''; $lowercollectionmeta = '';
 		foreach($response as $meta2){
