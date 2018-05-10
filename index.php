601802c1f5166bfef425f4a601bfb632d83905d1
info3g <infothreeg@gmail.com>
2018-05-10 12:58:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ $.ajax({
 	}
 });
 $('body').on('click', '.collectionSave', function(e) {
-var meta2 = $("#col-metafield2") $(this).val();
+var meta2 = $("#col-metafield2").val();
 alert(meta2);
 if(meta2 != '')	{
 	var colId = $(this).attr('data-id');
