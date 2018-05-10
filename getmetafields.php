0bc1f99aab6096a2649ae4f515bfaff53af17b25
info3g <infothreeg@gmail.com>
2018-05-10 11:19:46 +0530

    Update getmetafields.php

--- getmetafields.php
+++ getmetafields.php
@@ -8,12 +8,19 @@ echo $meta2 = $_REQUEST['meta2'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	//$shopify('POST /admin/collections/' + $collectionid + '/metafields.json',$metafield2);
-	$customresponse = $shopify('GET /admin/custom_collections' + $collectionid + '/metafields.json',$metafield2);
-	$smartresponse = $shopify('GET /admin/custom_collections' + $collectionid + '/metafields.json',$metafield2);
+	$response = $shopify('GET /admin/custom_collections' + $collectionid + '/metafields.json',$metafield2);
+	$smartresponse = $shopify('GET /admin/smart_collections' + $collectionid + '/metafields.json',$metafield2);
 	print_r($customresponse);
 	print_r($smartresponse);
 		$collectiondescription = ''; $lowercollectionmeta = '';
 		foreach($response as $meta2){
+			if($meta2['namespace'] == 'lowercollectionmeta'){
+				$lowercollectionmeta = $meta2['value'];
+			} else if($meta2['namespace'] == 'lowercollectionmeta'){
+				$lowercollectionmeta = $meta2['value'];
+			}
+		}
+	foreach($smartresponse as $meta2){
 			if($meta2['namespace'] == 'lowercollectionmeta'){
 				$lowercollectionmeta = $meta2['value'];
 			} else if($meta2['namespace'] == 'lowercollectionmeta'){
