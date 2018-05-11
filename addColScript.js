65c7109e3c0c97964711b554e897781e67d27f96
info3g <infothreeg@gmail.com>
2018-05-11 13:11:22 +0530

    Update addColScript.js

--- addColScript.js
+++ addColScript.js
@@ -3,6 +3,7 @@
     console.log(data);
     alert(12);
     var metaData = "{{ collection.metafields.collectionlower.lowerdata }}";
+    alert(window.location.href);
     console.log(metaData);
     $('.collection-description').after(metaData);
     $('.collection-hero').after('sdvsdvdsvdvdv');
