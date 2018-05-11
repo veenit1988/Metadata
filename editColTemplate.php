189a8dc16744e161313867514835e66aa1ad1603
info3g <infothreeg@gmail.com>
2018-05-11 18:08:34 +0530

    Update editColTemplate.php

--- editColTemplate.php
+++ editColTemplate.php
@@ -2,50 +2,47 @@
 require __DIR__.'/conf.php'; //Configuration
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
-error_reporting(E_ALL); 
-ini_set('display_errors', 1);
 $access_token = $_REQUEST['access_token'];
-echo $shop = $_REQUEST['shop'];
+$shop = $_REQUEST['shop'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
 	$themes = $shopify("GET /admin/themes.json");
-	print_r($themes);
 	foreach($themes as $theme) {
-	  if($theme['role'] == 'main') {
-	     $themeId = $theme['id'];
-	     $collectionTemplate = $shopify("GET /admin/themes/$themeId/assets.json?asset[key]=templates/collection.liquid&theme_id=$themeId");
-	     if (strpos($collectionTemplate['value'], '{{ collection.metafields.collectionlower.lowerdata }}') === false) {
-	       $colMeta = $collectionTemplate['value'].'{{ collection.metafields.collectionlower.lowerdata }}';
-	       $fields = array( "asset" => array('key' => 'templates/collection.liquid', 'value' => $colMeta));
-	       $jsonfields = json_encode($fields);
-               //$modifyColTemplate = $shopify("PUT /admin/themes/$themeId/assets.json");
-	       $curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://$shop/admin/themes/$themeId/assets.json",
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_CUSTOMREQUEST => "PUT",
-			CURLOPT_POSTFIELDS => $jsonfields,
-			CURLOPT_HTTPHEADER => array(
-				"cache-control: no-cache",
-				"content-type: application/json",
-				"x-shopify-access-token: $access_token"
-			),
-		));
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
-		curl_close($curl);
-		if ($err) {
-		//echo "cURL Error #:" . $err;
-		echo 'Try again Later!';
-		} else {
-		//echo $response;
-		echo 'Added Metafield Code!';
+		if($theme['role'] == 'main') {
+		$themeId = $theme['id'];
+		$collectionTemplate = $shopify("GET /admin/themes/$themeId/assets.json?asset[key]=templates/collection.liquid&theme_id=$themeId");
+			if (strpos($collectionTemplate['value'], '{{ collection.metafields.collectionlower.lowerdata }}') === false) {
+				$colMeta = $collectionTemplate['value'].'{{ collection.metafields.collectionlower.lowerdata }}';
+				$fields = array( "asset" => array('key' => 'templates/collection.liquid', 'value' => $colMeta));
+				$jsonfields = json_encode($fields);
+				//$modifyColTemplate = $shopify("PUT /admin/themes/$themeId/assets.json");
+				$curl = curl_init();
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => "https://$shop/admin/themes/$themeId/assets.json",
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_CUSTOMREQUEST => "PUT",
+					CURLOPT_POSTFIELDS => $jsonfields,
+					CURLOPT_HTTPHEADER => array(
+					"cache-control: no-cache",
+					"content-type: application/json",
+					"x-shopify-access-token: $access_token"
+					),
+				));
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
+				curl_close($curl);
+				if ($err) {
+					//echo "cURL Error #:" . $err;
+					echo 'Try again Later!';
+				} else {
+					//echo $response;
+					echo 'Added Metafield Code!';
+				}
+			} else {
+				echo 'Already Added Metafield Code!';
+			}
 		}
-	       }
-	  }
-	} else {
-	  echo 'Already added Metafield Code!';
 	}
 }
 catch (shopify\ApiException $e)
