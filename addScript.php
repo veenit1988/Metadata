0fd1ceeb6cdb8a0684fb3b24c549b7efae13e8f0
info3g <infothreeg@gmail.com>
2018-05-11 11:31:08 +0530

    Update addScript.php

--- addScript.php
+++ addScript.php
@@ -8,14 +8,37 @@ try
 {	
 	echo $url = "/admin/script_tags.json?src=".APP_SERVER_URL."addColScript.js?access_token=$access_token";
 	echo $js_file = APP_SERVER_URL."addColScript.js?access_token=$access_token";
-	$data = $shopify("GET $url");
-	if(!$data){
+	$JSdata = $shopify("GET $url");
+	if(!$JSdata){
 		$fields = array( "script_tag" => array('event' => 'onload', 'src' => $js_file));
-		$response = $shopify('POST /admin/script_tags.json',$fields);
-		print_r($response);
-		print_r('Add JS file');
+		//$response = $shopify('POST /admin/script_tags.json',$fields);
+		$jsonfields = json_encode($fields);
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://'.$_REQUEST['shop'].'/admin/script_tags.json',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => $jsonfields,
+			CURLOPT_HTTPHEADER => array(
+				"cache-control: no-cache",
+				"content-type: application/json",
+				"x-shopify-access-token: $access_token"
+			),
+		));
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) {
+		//echo "cURL Error #:" . $err;
+		echo 'Try again Later!';
+		} else {
+		echo $response;
+		echo 'JS file Created!';
+		}
+		
+		
 	} else {
-		//print_r($data);
+		print_r($JSdata);
 		print_r('Already exist JS file');
 	}
 	echo "<script src='".APP_SERVER_URL."addColScript.js?access_token=$access_token&shop=$shop'></script>";
