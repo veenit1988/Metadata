80dd29f8b170480548f9d1260091f802c943b30a
info3g <infothreeg@gmail.com>
2018-05-11 17:59:30 +0530

    Update editColTemplate.php

--- editColTemplate.php
+++ editColTemplate.php
@@ -4,7 +4,6 @@ require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
 echo $shop = $_REQUEST['shop'];
-/*
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
@@ -52,5 +51,5 @@ catch (shopify\ApiException $e)
 	echo $e;
 	print_r($e->getRequest());
 	print_r($e->getResponse());
-}*/
+}
 ?>
