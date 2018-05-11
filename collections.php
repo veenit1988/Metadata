f3b865b82ac9627f7c2c3b08561d0709cdffaa49
info3g <infothreeg@gmail.com>
2018-05-11 17:23:41 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -6,50 +6,52 @@ $access_token = $_REQUEST['access_token'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {     
-$collections = $shopify('GET /admin/custom_collections.json');
-$smartcollection= $shopify('GET /admin/smart_collections.json'); 
-	if($collections){
-	echo '<form method="post" name="form" id="getcollection" action="#">';
-	echo '<table cellspacing="10" cellpadding="10" border="1">';
-	echo '<thead><tr><th>Collection</th><th>Collection bottom content</th><th>Action</th></tr></thead>';
+	$collections = $shopify('GET /admin/custom_collections.json');
+	$smartcollection = $shopify('GET /admin/smart_collections.json'); 
+	if($collections || $smartcollection){
+		echo '<form method="post" name="form" id="getcollection" action="#">';
+		echo '<table cellspacing="10" cellpadding="10" border="1">';
+		echo '<thead><tr><th>Collection</th><th>Collection bottom content</th><th>Action</th></tr></thead>';
 		echo '<tbody>';
-	foreach($collections as $Allcollections)
-	{	
-		$img = '';
-		if($Allcollections['image']) {
-			$img = '<img src="'.$Allcollections["image"]["src"].'" alt="collectionimage" />';
-		}
-		$colId = $Allcollections["id"];
-		echo '<tr>';
-		echo '<td>'.$img.'<p class="col-title">'.$Allcollections['title'].'</p></td>';
-		echo '<td>'.'<textarea class="form-control" id="col-metafield2_'.$colId.'" name="lowerData[]"></textarea>'.'</td>';
-		echo '<td>'.'<input type="button" class="collectionSave" value="Save" name="addColData" data-id="'.$colId.'"></td>';
-		echo '</tr>';
+	}
+	if($collections){
+		foreach($collections as $Allcollections)
+		{	
+			$img = '';
+			if($Allcollections['image']) {
+				$img = '<img src="'.$Allcollections["image"]["src"].'" alt="collectionimage" />';
+			}
+			$colId = $Allcollections["id"];
+			echo '<tr>';
+			echo '<td>'.$img.'<p class="col-title">'.$Allcollections['title'].'</p></td>';
+			echo '<td>'.'<textarea class="form-control" id="col-metafield2_'.$colId.'" name="lowerData[]"></textarea>'.'</td>';
+			echo '<td>'.'<input type="button" class="collectionSave" value="Save" name="addColData" data-id="'.$colId.'"></td>';
+			echo '</tr>';
 
 		}
-	foreach($smartcollection as $smartcollections)
-	{	
-		$img = '';
-		if($smartcollections['image']) {
-			$img = '<img src="'.$smartcollections["image"]["src"].'" alt="collectionimage" />';
+	}
+	if($smartcollection){
+		foreach($smartcollection as $smartcollections)
+		{	
+			$img = '';
+			if($smartcollections['image']) {
+				$img = '<img src="'.$smartcollections["image"]["src"].'" alt="collectionimage" />';
+			}
+			$colId = $smartcollections["id"];
+			echo '<tr>';
+			echo '<td>'.$img.'<p class="col-title">'.$smartcollections['title'].'</p></td>';
+			echo '<td>'.'<textarea class="form-control" id="col-metafield2_'.$colId.'" name="lowerData[]"></textarea>'.'</td>';
+			echo '<td>'.'<input type="button" class="collectionSave" value="Save" name="addColData" data-id="'.$colId.'"></td>';
+			echo '</tr>';
 		}
-		$colId = $smartcollections["id"];
-		echo '<tr>';
-		echo '<td>'.$img.'<p class="col-title">'.$smartcollections['title'].'</p></td>';
-		echo '<td>'.'<textarea class="form-control" id="col-metafield2_'.$colId.'" name="lowerData[]"></textarea>'.'</td>';
-		echo '<td>'.'<input type="button" class="collectionSave" value="Save" name="addColData" data-id="'.$colId.'"></td>';
-		echo '</tr>';
-
-		}	
-		
-	echo '</tbody>';
-	echo '</table>';
- echo '</form>';
-}
-else {
-echo "<div class='no-result'>No collections</div>";
-}
-
+	}
+	if($collections || $smartcollection){
+		echo '</tbody>';
+		echo '</table>';
+		echo '</form>';
+	} else {
+		echo "<div class='no-result'>No collections</div>";
+	}
 }
 catch (shopify\ApiException $e)
 {
