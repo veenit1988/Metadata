6fd9abcc9dd8db238eb86149fcbabd7ca34057ba
info3g <infothreeg@gmail.com>
2018-05-11 15:38:46 +0530

    Update addScript.php

--- addScript.php
+++ addScript.php
@@ -7,8 +7,6 @@ $shop = $_REQUEST['shop'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
-	//$url = "/admin/script_tags.json?src=".APP_SERVER_URL."addColScript.js?access_token=$access_token&shop=$shop";
-	//$js_file = APP_SERVER_URL."addColScript.js?access_token=$access_token&shop=$shop";
 	$themes = $shopify("GET /admin/themes.json");
 	foreach($themes as $theme) {
 	  if($theme['role'] == 'main') {
@@ -35,44 +33,17 @@ try
 		$err = curl_error($curl);
 		curl_close($curl);
 		if ($err) {
-		echo "cURL Error #:" . $err;
-		} else {
-		echo $response;
-		}
-	       }
-	  }
-	}
-	echo 'Get JS data';
-	/*if(!$JSdata){
-		$fields = array( "script_tag" => array('event' => 'onload', 'src' => $js_file));
-		//$response = $shopify('POST /admin/script_tags.json',$fields);
-		$jsonfields = json_encode($fields);
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => 'https://'.$_REQUEST['shop'].'/admin/script_tags.json',
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_CUSTOMREQUEST => "POST",
-			CURLOPT_POSTFIELDS => $jsonfields,
-			CURLOPT_HTTPHEADER => array(
-				"cache-control: no-cache",
-				"content-type: application/json",
-				"x-shopify-access-token: $access_token"
-			),
-		));
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
-		curl_close($curl);
-		if ($err) {
 		//echo "cURL Error #:" . $err;
 		echo 'Try again Later!';
 		} else {
 		//echo $response;
-		echo 'JS file Created!';
+		echo 'Added Metafield Code!';
 		}
+	       }
+	  }
 	} else {
-		//print_r($JSdata);
-		print_r('Already exist JS file');
-	}*/
+	  echo 'Already added Metafield Code!';
+	}
 }
 catch (shopify\ApiException $e)
 {
