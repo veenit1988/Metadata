9078aabba05c637a096e6fbc916ad2747b35b71b
info3g <infothreeg@gmail.com>
2018-05-11 11:15:57 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -2,6 +2,6 @@
        
 	define('SHOPIFY_APP_API_KEY', 'a1d8a4ace6eaa1e0241afa2dcd19132c');
 	define('SHOPIFY_APP_SHARED_SECRET', 'fdf3bce4b18ab3c6bf490118795dda47');
-	
+	define('APP_SERVER_URL','https://collectionpage-customize.herokuapp.com/');
 ?>
 
