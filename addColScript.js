1382030d8acbb0781837b58dbf63d02373cf2cf0
info3g <infothreeg@gmail.com>
2018-05-11 13:16:54 +0530

    Update addColScript.js

--- addColScript.js
+++ addColScript.js
@@ -1,4 +1,8 @@
 
-//var data = $("script[src*='addColScript.js']").attr('src').split('?')[1];
-//console.log(data);
+var data = $("script[src*='addColScript.js']").attr('src').split('?')[1];
+console.log(data);
 alert(12);
+var metaData = "{{ collection.metafields.collectionlower.lowerdata }}";
+console.log(metaData);
+$('.collection-description').after(metaData);
+$('.collection-hero').after('sdvsdvdsvdvdv');
