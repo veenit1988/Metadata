7a487d3dffe36f035b6a3b1c30f519bf2f35dbbd
info3g <infothreeg@gmail.com>
2018-05-11 10:33:40 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -12,15 +12,10 @@ try
 	//Collection Metafield
 	$jsonmetafield = json_encode($metafield);
 	//$response = $shopify('POST /admin/collections/'.$collectionid.'/metafields.json',$metafield);
-	echo $_REQUEST['shop'].'/admin/collections/'.$collectionid.'/metafields.json';
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
-	  CURLOPT_URL => $_REQUEST['shop'].'/admin/collections/'.$collectionid.'/metafields.json',
+	  CURLOPT_URL => 'https://'.$_REQUEST['shop'].'/admin/collections/'.$collectionid.'/metafields.json',
 	  CURLOPT_RETURNTRANSFER => true,
-	  CURLOPT_ENCODING => "",
-	  CURLOPT_MAXREDIRS => 10,
-	  CURLOPT_TIMEOUT => 30,
-	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	  CURLOPT_CUSTOMREQUEST => "POST",
 	  CURLOPT_POSTFIELDS => $jsonmetafield,
 	  CURLOPT_HTTPHEADER => array(
